bbe2cbe0b4361e225cf2ca36868fda686f86cafc
heigen007 <67007974+Heigen007@users.noreply.github.com>
2023-04-08 01:27:22 +0600

    Update

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const express = require("express");
-require('dotenv').config()
-const { Configuration, OpenAIApi } = require("openai");
-const cors = require("cors")
+import express from "express";
+import * as dotenv from 'dotenv'
+import midjourney from "midjourney-client"
+dotenv.config()
+import { Configuration, OpenAIApi } from "openai";
+import cors from "cors"
 const app = express();
 const PORT = 3000
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use(cors())
 app.use(express.json());
@@ -31,7 +37,12 @@ app.post("/makeRequest", async (req, res) => {
     var result = await makeRequest(req.body.question)
     res.send({ answer: result.choices[0].text })
 })
+
+app.post("/photo", async (req, res) => {
+    var result = await midjourney(req.body.question)
+    res.send({ result })
+})
  
 app.listen(PORT, () => console.log("Started on port " + PORT));
 
-module.exports = app;
+export default app
